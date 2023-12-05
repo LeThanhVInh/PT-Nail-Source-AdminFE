01f0de14ea8f73841ab67e509de45cc5f86c8880
LeThanhVInh <levinh1997.vl@gmail.com>
2023-12-05 14:38:31 +0700

    Đổi Modal sang dạng Ref

--- src/providers/constants.js
+++ src/providers/constants.js
@@ -1,3 +1,10 @@
+let apiUrl = "https://phanmemvas.vn:444";
+
+const exportedObject = {
+    'apiUrl': apiUrl,
+};
+export default exportedObject
+
 export const modalSizes = {
     mini: "360px",
     tiny: "540px",
@@ -5,3 +12,16 @@ export const modalSizes = {
     large: "1080px",
     full: "auto",
 };
+
+export const getSizeOfModal = (type) => {
+    if (type === modalSizes.mini && window.innerWidth < 360)
+        return modalSizes.full;
+    else if (type === modalSizes.tiny && window.innerWidth < 540)
+        return modalSizes.full;
+    else if (type === modalSizes.medium && window.innerWidth < 720)
+        return modalSizes.full;
+    else if (type === modalSizes.large && window.innerWidth < 1080)
+        return modalSizes.full;
+    else
+        return type.toString();
+}
